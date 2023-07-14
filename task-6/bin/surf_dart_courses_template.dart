cd2af66d70caa784f163c9370a1f08a60d91d984
Andrei <zariciuc.andrei@gmail.com>
2023-07-14 10:39:26 +0300

    Resolved task-6

--- task-6/bin/surf_dart_courses_template.dart
+++ task-6/bin/surf_dart_courses_template.dart
@@ -1,5 +1,46 @@
-import 'package:surf_dart_courses_template/surf_dart_courses_template.dart' as surf_dart_courses_template;
+import 'package:surf_dart_courses_template/surf_dart_courses_template.dart'
+    as surf_lib;
 
 void main(List<String> arguments) {
-  print('Hello world: ${surf_dart_courses_template.calculate()}!');
+  // Список товаров хранится в строковом виде
+  final articles = '''
+1,хлеб,Бородинский,500,5
+2,хлеб,Белый,200,15
+3,молоко,Полосатый кот,50,53
+4,молоко,Коровка,50,53
+5,вода,Апельсин,25,100
+6,вода,Бородинский,500,5
+''';
+
+  // Получить список товаров
+  final products = surf_lib.convertToList(
+    articles,
+    surf_lib.ProductListFromStringConverter(),
+  );
+
+  // Применить фильтр по категории
+  final filteredProductsByCategory = surf_lib.applyFilter(
+    products,
+    surf_lib.FilterProductByCategory('молоко'),
+  );
+  // вывод данных
+  surf_lib.printProductList(filteredProductsByCategory, '\nКатегория "молоко"');
+
+  // Применить фильтр по цене
+  final filteredProductsByPrice = surf_lib.applyFilter(
+    products,
+    surf_lib.FilterProductByPriceNotHigher(200),
+  );
+  // вывод данных
+  surf_lib.printProductList(
+      filteredProductsByPrice, '\nТовары с ценой не больше 200 рублей');
+
+  // Применить фильтр по остатку на складе
+  final filteredProductsByQuantity = surf_lib.applyFilter(
+    products,
+    surf_lib.FilterProductByQuantitySmaller(50),
+  );
+  // вывод данных
+  surf_lib.printProductList(
+      filteredProductsByQuantity, '\nТовары с остатком меньше 50 шт');
 }
